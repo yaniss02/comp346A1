54eb0e3cfb37959d91ec078b762c85b3f62b2280
yaniss02 <kherchachey@gmail.com>
2025-02-09 17:27:20 -0500

    deadlock

---
+++ Driver.java
@@ -0,0 +1,35 @@
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Kerly Titus
+ */
+public class Driver {
+
+    /** 
+     * main class
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    	
+    	 /*******************************************************************************************************************************************
+    	  * TODO : implement all the operations of main class   																					*
+    	  ******************************************************************************************************************************************/
+        
+    	Network objNetwork = new Network("network");            /* Activate the network */
+        objNetwork.start();
+        Server objServer = new Server();  
+        Client sendingClient = new Client("sending");
+        sendingClient.start();
+        objServer.start();   
+        Client receivingClient = new Client("receiving");
+        receivingClient.start();
+        
+        /* Complete here the code for the main method ...*/
+    }
+}
